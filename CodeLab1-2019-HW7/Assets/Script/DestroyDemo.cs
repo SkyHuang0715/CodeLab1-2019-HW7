c51ec9b59665bdd2065fae9a2bc4dbaef6ab369b
SkyHuang0715 <she515996344@gmail.com>
2019-04-27 03:51:43 -0400

    add materials

---
+++ CodeLab1-2019-HW7/Assets/Script/DestroyDemo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DestroyDemo : MonoBehaviour {
+ 
+    //每个砖块的自动销毁脚本
+ 
+    //AudioSource asbro;
+    // Use this for initialization
+    void Start () {
+        //asbro = GetComponent<AudioSource>();
+    }
+	
+    // Update is called once per frame
+    void Update () {
+		
+    }
+ 
+    private void OnCollisionEnter(Collision collision)
+    {
+        //asbro.Play();
+        Destroy(this.gameObject,0.05f);   //碰撞后 0.05s销毁自身
+    }
+}
+
